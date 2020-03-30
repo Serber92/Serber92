2b9b1a09ccd27d38ca079e5481386f90a2f1530a
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2020-03-29 20:10:55 -0700

    added index page and routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path     
 from . import views
+
 urlpatterns = [
-    path('', views.index),	   
+    path('', views.index),
+    path('add_ingredient', views.add_ingredient),
+    path('reset', views.reset),  
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
+  if not 'ingredients_list' in request.session:
+    request.session['ingredients_list'] = []
   return render(request, "index.html")
+  
+def add_ingredient(request):
+  ingredient = request.POST['ingredient']
+  if not 'ingredients_list' in request.session:
+    request.session['ingredients_list'] = []
+  request.session['ingredients_list'].append(ingredient)
+  request.session.modified = True
+  return redirect("/")
 
+def reset(request):
+  request.session['ingredients_list'] = []
+  return redirect("/")
 
