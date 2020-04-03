fcec5fd5d2155117387273c7476ee70905ccda18
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2020-04-02 20:52:59 -0700

    mpdulared code

--- main_app/views.py
+++ main_app/views.py
@@ -7,25 +7,34 @@ def index(request):
   return render(request, "index.html")
   
 def add_ingredient(request):
-  ingredient = request.POST['ingredient']
-  if not 'ingredients_list' in request.session:
-    request.session['ingredients_list'] = []
-  request.session['ingredients_list'].append(ingredient)
-  request.session.modified = True
-  if request.session['ingredients_list']:
-    base_url = 'https://api.spoonacular.com/recipes'
-    search_ingr = '/findByIngredients'
-    params = {'number':3, 'apiKey':'1f885b8157ab4f0e8c5cf1e6e8b1849f'}
-    for ingr in request.session['ingredients_list']:
-      if not 'ingredients' in params:
-        params['ingredients'] = ingr
-      else:
-        params['ingredients'] += ',' + ingr
-    req = requests.get(url = base_url + search_ingr, params = params)
-    data = req.json()
+  addIngredientToSession(request)
+  getRecipes(request.session['ingredients_list'])
   return redirect("/")
 
 def reset(request):
   request.session['ingredients_list'] = []
   return redirect("/")
 
+
+
+# help functions
+
+def addIngredientToSession(request):
+  ingredient = request.POST['ingredient']
+  if not 'ingredients_list' in request.session:
+    request.session['ingredients_list'] = []
+  request.session['ingredients_list'].append(ingredient)
+  request.session.modified = True
+
+def getRecipes(ingredients):
+  base_url = 'https://api.spoonacular.com/recipes'
+  search_ingr = '/findByIngredients'
+  params = {'number':3, 'apiKey':'1f885b8157ab4f0e8c5cf1e6e8b1849f'}
+  for ingr in ingredients:
+    if not 'ingredients' in params:
+      params['ingredients'] = ingr
+    else:
+      params['ingredients'] += ',' + ingr
+  req = requests.get(url = base_url + search_ingr, params = params)
+  data = req.json()
+  return data
