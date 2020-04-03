b238972d8a8d04f21cee63d95706aa7b2b932636
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2020-04-02 20:38:42 -0700

    added Django env and fixed request

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
+import requests
 
 def index(request):
   if not 'ingredients_list' in request.session:
@@ -11,6 +12,17 @@ def add_ingredient(request):
     request.session['ingredients_list'] = []
   request.session['ingredients_list'].append(ingredient)
   request.session.modified = True
+  if request.session['ingredients_list']:
+    base_url = 'https://api.spoonacular.com/recipes'
+    search_ingr = '/findByIngredients'
+    params = {'number':3, 'apiKey':'1f885b8157ab4f0e8c5cf1e6e8b1849f'}
+    for ingr in request.session['ingredients_list']:
+      if not 'ingredients' in params:
+        params['ingredients'] = ingr
+      else:
+        params['ingredients'] += ',' + ingr
+    req = requests.get(url = base_url + search_ingr, params = params)
+    data = req.json()
   return redirect("/")
 
 def reset(request):
