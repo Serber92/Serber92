0f994c9b616b4d05b5816b64ab0e66662512d124
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2020-04-07 10:15:18 -0700

    added recepie search by ingredients

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render, HttpResponse, redirect
 import requests
 
+base_url = 'https://api.spoonacular.com/recipes'
+
 def index(request):
   if not 'ingredients_list' in request.session:
     request.session['ingredients_list'] = []
+  if not 'cards_list' in request.session:
+    request.session['cards_list'] = []
   return render(request, "index.html")
   
 def add_ingredient(request):
   addIngredientToSession(request)
-  getRecipes(request.session['ingredients_list'])
+  recipes = getRecipes(request.session['ingredients_list'])
+  cards = recipeCards(recipes)
+  request.session['cards_list'] = cards
+  request.session.modified = True
   return redirect("/")
 
 def reset(request):
   request.session['ingredients_list'] = []
+  request.session['cards_list'] = []
   return redirect("/")
 
   
@@ -27,14 +35,41 @@ def addIngredientToSession(request):
   request.session.modified = True
 
 def getRecipes(ingredients):
-  base_url = 'https://api.spoonacular.com/recipes'
-  search_ingr = '/findByIngredients'
+  endpoint = '/findByIngredients'
   params = {'number':3, 'apiKey':'1f885b8157ab4f0e8c5cf1e6e8b1849f'}
   for ingr in ingredients:
     if not 'ingredients' in params:
       params['ingredients'] = ingr
     else:
       params['ingredients'] += ',' + ingr
-  req = requests.get(url = base_url + search_ingr, params = params)
-  data = req.json()
+  request = requests.get(url=base_url+endpoint, params = params)
+  data = request.json()
   return data
+
+def recipeCards(recipes):
+  Cards = []
+  for recipe in recipes:
+    image = recipe['image']
+    title = recipe['title']
+    id = recipe['id']
+    additionalIngr = []
+    for ingredient in recipe["missedIngredients"]:
+      additionalIngr.append(ingredient['originalString'])
+    Cards.append({
+      "title":title,
+      "image":image,
+      "url":getRecipeLink(id),
+      "additionalIngr":additionalIngr
+    })
+  return Cards
+
+def getRecipeLink(id):
+  endpoint = '/information'
+  params = {'apiKey':'1f885b8157ab4f0e8c5cf1e6e8b1849f', 'includeNutrition':'false'}
+  request = requests.get(url=base_url+'/'+str(id)+endpoint, params=params)
+  data = request.json()
+  url = data['sourceUrl']
+  return url
+
+  
+  
